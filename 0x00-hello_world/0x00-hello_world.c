98953c536adbbd30738945fb40a8803ce2ea9c9a
GiphtyMens <giftymens111@gmail.com>
2022-07-02 08:15:30 -0700

    hi

---
+++ 0x00-hello_world/0x00-hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Prints print size of data types
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammer but the outcome is a piece of art,\n");
+return (0);
+}
